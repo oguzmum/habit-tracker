5e179bae0f85c388d10248aabc830759ff9f233b
Oguz Mumcu <oguzmum@gmail.com>
2025-07-07 23:44:22 +0200

    init

---
+++ src/test/java/com/ozzo/habit_tracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozzo.habit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
